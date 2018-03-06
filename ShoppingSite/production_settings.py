5474d454f93b6c7ed311a7b6dc4eb55a951a4b6e
nekosgr93 <zerofoxy05@gmail.com>
2018-03-06 20:21:29 +0800

    fuck

--- ShoppingSite/production_settings.py
+++ ShoppingSite/production_settings.py
@@ -9,7 +9,7 @@ DATABASES = {
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
 
 MEDIA_URL = '/media/'
--- ShoppingSite/wsgi.py
+++ ShoppingSite/wsgi.py
@@ -15,5 +15,5 @@ from dj_static import Cling
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "ShoppingSite.settings")
 
-application = get_wsgi_application()
+
 application = Cling(get_wsgi_application())
